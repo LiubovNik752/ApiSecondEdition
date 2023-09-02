b1247eb732b08751a3f6ef0509df6e89072732cc
Lyubov Baturina <121640778+LiubovNik752@users.noreply.github.com>
2023-09-02 17:35:27 +0300

    Add 1 file

--- test_ApiRequest/test_ApiRequest.py
+++ test_ApiRequest/test_ApiRequest.py
@@ -1,39 +1,44 @@
-from ApiRequest.ApiRequest import ApiRequest
+from ApiRequest.AR import ApiRequest
 
 
 class Test_ApiRequest:
+    """Тест-класс для класса ApiRequest"""
+
+    def test_class(self):
+        self.request1 = ApiRequest()
+        assert isinstance(self.request1, ApiRequest)
 
     def test_check_attribute(self):
-        self.request1 = ApiRequest()
-        assert hasattr(self.request1, 'payload')
-        assert hasattr(self.request1, 'request_type')
+        request1 = ApiRequest()
+        assert hasattr(request1, 'payload')
+        assert hasattr(request1, 'request_type')
+
+    def test_type(self):
+        value1 = "data"
+        self.request1 = ApiRequest(payload=value1)
+        assert type(self.request1.payload) == str
 
     def test_default(self):
         self.request1 = ApiRequest()
         assert self.request1.payload == ''
+        assert self.request1.request_type == ''
 
     def test_equality(self):
-        self.request1.payload = 'data'  # Проверка изменения атрибута payload через property
-        assert self.request1.payload == "data"
-        self.request2 = ApiRequest(payload = "some_data")
-        assert self.request2.payload == "some_data"
-        self.request3 = ApiRequest(payload = 123)
-        assert self.request3.payload == 123
-        self.request4 = ApiRequest(payload = 123)
-        assert self.request3.__dict__ == self.request4.__dict__
+        value1 = "some_data"
+        self.request1 = ApiRequest(payload = value1)
+        assert self.request1.payload == value1
+
 
     def test_inequality(self):
+        value1 = "data"
+        value2 = "new_data"
+        self.request1 = ApiRequest(payload=value1)
+        self.request2 = ApiRequest(payload=value2)
         assert self.request1 != self.request2
-        self.request5 = ApiRequest(payload = '123')
-        assert self.request4 != self.request5
 
-
-    def test_type(self):
-        assert type(self.request2.payload) == str
-        assert type(self.request3.payload) == int
-        assert isinstance(self.request1, ApiRequest)
 
     def test_method_set_payload(self):
-        self.request2.set_payload("new_data")
-        assert self.request2.payload != "some_data"
-        assert self.request2.payload == "new_data"
+        self.request1 = ApiRequest()
+        value = "new_data"
+        self.request1.set_payload(value)
+        assert self.request1.payload == value
